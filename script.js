88fc978d0a7ec5b087b8c06793418ba790466433
Rodrigo <rodrigotteixeira7@gmail.com>
2024-06-04 00:58:37 +0100

    fixed eval and added operator confirmation

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-input_to_eval = [];
-output = [];
-number1 = [];
-operation_status = 1;
+let input_to_eval = [];
+let output = [];
+let operation_status = 1;
 
 // Updates display
 function update_display() {
     document.getElementById("result").innerHTML = output.join("");
 }
 
+// Activates button
+function activate_button(operator) {
+    document.getElementById(operator).classList.add("active");
+}
+
+// Deactive all buttons
+function deactivate_all_buttons() {
+    let elements = document.getElementsByClassName("active");
+    for (i = 0; i < elements.length; i++) {
+        elements[i].classList.remove("active");
+    }
+}
+
 // Adds number to input
 function write_number(number) {
     if (operation_status) {
+        input_to_eval = [];  // Clear input to start a new calculation
         output = [];
         operation_status = 0;
     }
@@ -21,17 +34,34 @@ function write_number(number) {
 
 // Adds operator to input
 function write_operator(operator) {
-    input_to_eval.push(operator);
-    number1 = output;
-    output = [];
-    update_display();
+    if (operation_status === 0 && input_to_eval.length > 0 && !isNaN(input_to_eval[input_to_eval.length - 1])) {
+        activate_button(operator);
+        input_to_eval.push(operator);
+        output = [];
+        update_display();
+    }
 }
 
 // Evaluates input
 function calc() {
-    output[0] = eval(input_to_eval.join(""));
+    let result = eval(input_to_eval.join(""));
+    if (isFinite(result) === false) {
+        output = ["Math Error (/0)"];
+    } else {
+        output = [result];
+    }
+
+    input_to_eval = [result];
+    operation_status = 1;
+    deactivate_all_buttons();
+    update_display();
+}
+
+function clean() {
+    output = [];
     input_to_eval = [];
     operation_status = 1;
+    deactivate_all_buttons();
     update_display();
 }
 
@@ -44,4 +74,4 @@ window.onkeydown = function(event) {
     } else if (event.key === "Enter") {
         calc();
     }
-};
+}
